4839f32fed1957653a37160d50388f0d82a41413
Anton Babunov <anton.babunovv@gmail.com>
2023-08-03 11:51:07 +0200

    updated shit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21-600x900.jpg'
+    ),
+  ];
 }
