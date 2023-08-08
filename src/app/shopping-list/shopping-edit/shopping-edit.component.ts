56e9418b9ac19cde5c60b479bd39ba6b1bad879e
Anton Babunov <anton.babunovv@gmail.com>
2023-08-08 14:57:43 +0200

    template driven form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -7,24 +7,20 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 import { ShoppingListService } from '../shopping-list-service';
 
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) {}
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
+  onAddItem(form: NgForm) {
+    const ingName = form.value
 
-    if (ingName && ingAmount) {
-      const newIngredient = new Ingredient(ingName, ingAmount);
-      this.shoppingListService.addIngredient(newIngredient);
-    }
+    const newIngredient = new Ingredient(ingName.name, ingName.amount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
