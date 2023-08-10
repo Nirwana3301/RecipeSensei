2b6bf1c36d769925b77e9e88bc1b055b8fe81486
Anton Babunov <anton.babunovv@gmail.com>
2023-08-10 11:43:21 +0200

    added login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
 import { RecipesResolverService } from './recipes/recipe-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   {
@@ -44,7 +45,7 @@ const appRoutes: Routes = [
   },
   {
     path: 'auth',
-    
+    component: AuthComponent,
   },
 ];
 
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,27 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-auth',
-    templateUrl: './auth.component.html'
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+  isLoginMode = true;
 
+  authForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    console.log(this.authForm);
+    this.authForm.reset();
+  }
 }
